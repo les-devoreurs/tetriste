bd85b38f516b9a3d2dc2c0ae2f53074690e0d137
mina <minatouama@gmail.com>
2025-06-24 20:35:15 +0200

    update main

--- src/main.c
+++ src/main.c
@@ -112,66 +112,67 @@ int main(int argc, char** argv) {
 
     Tetromino best_move = {3, 3, 3, 3};
     
-    while (!quit && !statePlayer.game_over && !stateBot.game_over && !return_to_menu) {
-        handle_input(&event, &quit, &statePlayer, &return_to_menu); // Gestion des entrées clavier
-        
-        Uint32 now_player = SDL_GetTicks();
-        if (now_player - statePlayer.last_drop_time > statePlayer.drop_speed) {
-            int temp_lines_cleared_player = update_game(&statePlayer);  // Mise à jour de l'état du jeu
+        while (!global_quit && !statePlayer.game_over && !stateBot.game_over && !return_to_menu) {
+            handle_input(&event, &global_quit, &statePlayer, &return_to_menu); // Gestion des entrées clavier
+            
+            Uint32 now_player = SDL_GetTicks();
+            if (now_player - statePlayer.last_drop_time > statePlayer.drop_speed) {
+                int temp_lines_cleared_player = update_game(&statePlayer);  // Mise à jour de l'état du jeu
 
-            if(gamemode == 1 &&  temp_lines_cleared_player != -1) {
-                lines_cleared_player += temp_lines_cleared_player;
-                add_garbage_lines(&statePlayer, lines_cleared_bot);     // Ajoute une ligne dans la grille pour chaque ligne finit par le bot
-                lines_cleared_bot = 0;
-            }
-            statePlayer.last_drop_time = now_player;
-        }
-        
-        if (gamemode == 1) {
-            Uint32 now_bot = SDL_GetTicks();
-            if (now_bot - stateBot.last_drop_time > stateBot.drop_speed) {
-                best_move = find_best_move(&stateBot);
-                apply_move_bot(&stateBot, &best_move);
-                
-                int temp_lines_cleared_bot = update_game(&stateBot);                // Mise à jour de l'état du jeu
-                if (temp_lines_cleared_bot != -1) {
-                    lines_cleared_bot += temp_lines_cleared_bot;
-                    add_garbage_lines(&stateBot, lines_cleared_player);     // Ajoute une ligne dans la grille pour chaque ligne finit par le joueur 
-                    best_move.y -= lines_cleared_player;
-                    lines_cleared_player = 0;
+                if(gamemode == 1 &&  temp_lines_cleared_player != -1) {
+                    lines_cleared_player += temp_lines_cleared_player;
+                    add_garbage_lines(&statePlayer, lines_cleared_bot);     // Ajoute une ligne dans la grille pour chaque ligne finit par le bot
+                    lines_cleared_bot = 0;
                 }
-                stateBot.last_drop_time = now_bot;
+                statePlayer.last_drop_time = now_player;
             }
+            
+            if (gamemode == 1) {
+                Uint32 now_bot = SDL_GetTicks();
+                if (now_bot - stateBot.last_drop_time > stateBot.drop_speed) {
+                    best_move = find_best_move(&stateBot);
+                    apply_move_bot(&stateBot, &best_move);
+                    
+                    int temp_lines_cleared_bot = update_game(&stateBot);                // Mise à jour de l'état du jeu
+                    if (temp_lines_cleared_bot != -1) {
+                        lines_cleared_bot += temp_lines_cleared_bot;
+                        add_garbage_lines(&stateBot, lines_cleared_player);     // Ajoute une ligne dans la grille pour chaque ligne finit par le joueur 
+                        best_move.y -= lines_cleared_player;
+                        lines_cleared_player = 0;
+                    }
+                    stateBot.last_drop_time = now_bot;
+                }
+            }
+            
+            SDL_RenderClear(renderer);
+            draw_on_renderer(renderer, &statePlayer, font, viewport_player);  // Rendu graphique joueur
+            if (gamemode == 1)
+            {
+                draw_on_renderer(renderer, &stateBot, font, viewport_bot);        // Rendu graphique bot
+            }
+
+            // Afficher le résultat
+            SDL_RenderPresent(renderer);
+            SDL_Delay(16); // ~60 FPS
         }
-        
-        SDL_RenderClear(renderer);
-        draw_on_renderer(renderer, &statePlayer, viewport_player);  // Rendu graphique joueur
-        if (gamemode == 1)
-        {
-            draw_on_renderer(renderer, &stateBot, viewport_bot);        // Rendu graphique bot
+
+        // Game Over
+        if (gamemode == 1 && statePlayer.game_over) {
+            printf("Game Over! You lose against the bot!\n");
+            SDL_Delay(2000); // Attente avant de quitter
         }
-
-        // Afficher le résultat
-        SDL_RenderPresent(renderer);
-        SDL_Delay(16); // ~60 FPS
+        else if (gamemode == 1 && stateBot.game_over) {
+            printf("Win! You win against the bot!\n");
+            SDL_Delay(2000); // Attente avant de quitter
+        }
+        else if (gamemode == 0) {
+            printf("Game Over! Score:%u\n", statePlayer.score);
+        }
     }
-
-    // Game Over
-    if (gamemode == 1 && statePlayer.game_over) {
-        printf("Game Over! You lose against the bot!\n");
-        SDL_Delay(2000); // Attente avant de quitter
-    }
-    else if (gamemode == 1 && stateBot.game_over) {
-        printf("Win! You win against the bot!\n");
-        SDL_Delay(2000); // Attente avant de quitter
-    }
-    else if (gamemode == 0) {
-        printf("Game Over! Score:%u\n", statePlayer.score);
-    }
-
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
     TTF_CloseFont(font);
     return 0;
 }
+
